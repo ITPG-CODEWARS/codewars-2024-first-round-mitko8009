ff20c4e9a103dd59c03cdf5b6bc1ce387c118b46
mitko8009 <mitko8009.m@gmail.com>
2024-10-27 16:43:54 +0200

    added history page

--- i18n/es.js
+++ i18n/es.js
@@ -7,4 +7,8 @@ export default {
     "__map__": "Metro de Sofía - Mapa",
     "map_description": "El metro de Sofía tiene 4 líneas (M1, M2, M3, M4) y 47 estaciones. El sistema tiene una longitud de 52,8 km y tiene una afluencia diaria de más de 350k pasajeros.",
     "history": "Historia",
+    "history_title": "History of Sofia Metro",
+    "history_description": "Construction of the metro started in the 80s with the demolition of a significant number of buildings. At the beginning of the 90s, construction stopped due to a lack of funds and the complexity of the construction work. Being one of the oldest cities in Europe, Sofia contains many historical layers underneath its central areas. Evidence of antiquity can be clearly seen at the Serdika Station, which exhibits a wealth of unearthed Thracian and Roman ruins. During the construction of the enormous complex of the National Palace of Culture, two stations forming part of the M2 line and their connecting tunnels were built.",
+    "history_title2": "Rolling stock",
+    "history_description2": 'The system uses 3 types of rolling stock. The older train sets, type 81-717/714.4, were manufactured by Metrowagonmash in Mytishchi, Moscow Oblast, Russia and consists of 48 carriages in total. The second generation of rolling stock, type 81-740/741 "Rusich", were also manufactured by Metrowagonmash and delivered between 2005 and 2013, consisting of 120 carriages in total. The third generation of rolling stock, Siemens Inspiro 30 three-car sets were delivered between 2016 and 2021 and exclusively serve the M3 line.',
 }
