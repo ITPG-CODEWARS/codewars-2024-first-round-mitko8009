27ccd1ea7a14354cd83a88430360ec419824a8d7
mitko8009 <mitko8009.m@gmail.com>
2024-10-29 23:34:08 +0200

    Fixed Train pages

--- i18n/bg.js
+++ i18n/bg.js
@@ -13,4 +13,10 @@ export default {
     "history_description2": 'В системата се използват 3 вида подвижен състав. По-старите влакови композиции, тип 81-717/714.4, са произведени от Метровагонмаш в Митищи, Московска област, Русия и се състоят от общо 48 вагона. Второто поколение подвижен състав тип 81-740/741 „Русич“ също е произведено от Метровагонмаш и доставено между 2005 и 2013 г., състоящо се от общо 120 вагона. Третото поколение подвижен състав, комплектите от три вагона Siemens Inspiro 30 бяха доставени между 2016 г. и 2021 г. и обслужваха изключително линията M3.',
     "dido_e_vinoven_za_tozi_button_kojto_go_e_pisal_v_reglamenta_na_systezanieto_codewars_2024_2025": "Научете повече",
     "readMoreAbout": "Прочети повече за",
+    "model1": "81-717/714.4",
+    "model1_info": "The 81-717/81-714 is a Soviet/Russian metro car model and the most produced member of the 81-series, designed in the Soviet Union in the mid-1970s. The cars were made from 1976 to 2014 by Metrovagonmash and the I. E. Yegorov Vagonmash factories of Mytishchi and Saint Petersburg, respectively. Production is still ongoing for specific modifications, and is both the most produced metro train ever, currently being used in 11 countries worldwide, in 19 metropolitans, and having been produced in 24 different modifications in total, as well as the longest produced metro train ever, with over 7,000 cars having been produced in total so far.",
+    "model2": "81-740/741",
+    "model2_info": "The 81-740/81-741 Rusich is a model of metro car designed and built by Metrovagonmash. It is the second most common type of metro car in the world, after the 81-717/714.4. The first train was built in 2005 and the last in 2013. The train is used in 12 countries and 20 cities. The train is used in the Sofia Metro, the only metro system in Bulgaria.",
+    "model3": "Siemens Inspiro 30",
+    "model3_info": "The Siemens Inspiro is a type of metro train designed and manufactured by Siemens Mobility. The first Inspiro entered service on Warsaw Metro's Line M1 in 2012. The trains are used in 14 countries and 17 cities. The trains are used in the Sofia Metro, the only metro system in Bulgaria.",
 }
